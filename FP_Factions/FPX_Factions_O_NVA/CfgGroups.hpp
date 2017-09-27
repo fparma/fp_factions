4b0a533f8a13a845a84762831e285fc8b283ac66
Cuel <cuel@users.noreply.github.com>
2017-09-27 20:53:33 +0200

    Fix removal of wrong curly brace

--- FP_Factions/FPX_Factions_O_NVA/CfgGroups.hpp
+++ FP_Factions/FPX_Factions_O_NVA/CfgGroups.hpp
@@ -213,276 +213,277 @@ class East {
 					position[] = {5,-4,0};
 				};
 			};
-			class SpecialForces {
-				name = "Vietcong";
-				class FPX_Factions_O_NVA_VC_Squad {
-					name = "Vietcong Squad";
+		};
+
+		class SpecialForces {
+			name = "Vietcong";
+			class FPX_Factions_O_NVA_VC_Squad {
+				name = "Vietcong Squad";
+				side = 0;
+				faction = "FPX_O_NVA";
+				class Unit0 {
 					side = 0;
-					faction = "FPX_O_NVA";
-					class Unit0 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_CL";
-						rank = "SERGEANT";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila";
-						rank = "CORPORAL";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
-					class Unit3 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_MG_DP27";
-						rank = "PRIVATE";
-						position[] = {10,-10,0};
-					}; 
-					class Unit4 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "PRIVATE";
-						position[] = {-10,-10,0};
-					};
-					class Unit5 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "PRIVATE";
-						position[] = {15,-15,0};
-					};
-					class Unit6 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "PRIVATE";
-						position[] = {20,-20,0};
-					};
-					class Unit7 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AVT";
-						rank = "PRIVATE";
-						position[] = {-20,-20,0};
-					};
-					class Unit8 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_AT";
-						rank = "PRIVATE";
-						position[] = {-25,-25,0};
-					};
+					vehicle = "FPX_O_NVA_VC_CL";
+					rank = "SERGEANT";
+					position[] = {0,0,0};
 				};
-				class FPX_Factions_O_NVA_VC_RaidingParty {
-					name = "Vietcong Raiders";
+				class Unit1 {
 					side = 0;
-					faction = "FPX_O_NVA";
-					class Unit0 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_CL";
-						rank = "SERGEANT";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "CORPORAL";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
-					class Unit3 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "PRIVATE";
-						position[] = {10,-10,0};
-					}; 
-					class Unit4 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Raider";
-						rank = "PRIVATE";
-						position[] = {-10,-10,0};
-					};
-					class Unit5 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Raider";
-						rank = "PRIVATE";
-						position[] = {15,-15,0};
-					};
-					class Unit6 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Raider";
-						rank = "PRIVATE";
-						position[] = {20,-20,0};
-					};
-					class Unit7 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_MG_DP27";
-						rank = "PRIVATE";
-						position[] = {-20,-20,0};
-					};
-					class Unit8 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_AT";
-						rank = "PRIVATE";
-						position[] = {-25,-25,0};
-					};
+					vehicle = "FPX_O_NVA_VC_Guerrila";
+					rank = "CORPORAL";
+					position[] = {5,-5,0};
 				};
-				class FPX_Factions_O_NVA_VC_CityFighters {
-					name = "Vietcong City Fighters";
+				class Unit2 {
 					side = 0;
-					faction = "FPX_O_NVA";
-					class Unit0 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_CL";
-						rank = "SERGEANT";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "CORPORAL";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
-					class Unit3 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_MG_MG42";
-						rank = "PRIVATE";
-						position[] = {10,-10,0};
-					}; 
-					class Unit4 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_CQC";
-						rank = "PRIVATE";
-						position[] = {-10,-10,0};
-					};
-					class Unit5 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_CQC";
-						rank = "PRIVATE";
-						position[] = {15,-15,0};
-					};
-					class Unit6 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_CQC";
-						rank = "PRIVATE";
-						position[] = {20,-20,0};
-					};
-					class Unit7 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_AT";
-						rank = "PRIVATE";
-						position[] = {-20,-20,0};
-					};
-					class Unit8 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_AT";
-						rank = "PRIVATE";
-						position[] = {-25,-25,0};
-					};
+					vehicle = "FPX_O_NVA_VC_Guerrila";
+					rank = "PRIVATE";
+					position[] = {-5,-5,0};
 				};
-				class FPX_Factions_O_NVA_VC_Cell {
-					name = "Vietcong Cell";
+				class Unit3 {
 					side = 0;
-					faction = "FPX_O_NVA";
-					class Unit0 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "CORPORAL";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_MG_DP27";
-						rank = "PRIVATE";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
+					vehicle = "FPX_O_NVA_VC_MG_DP27";
+					rank = "PRIVATE";
+					position[] = {10,-10,0};
+				}; 
+				class Unit4 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "PRIVATE";
+					position[] = {-10,-10,0};
 				};
-				class FPX_Factions_O_NVA_VC_ATCell {
-					name = "Vietcong AT Cell";
+				class Unit5 {
 					side = 0;
-					faction = "FPX_O_NVA";
-					class Unit0 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
-						rank = "CORPORAL";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_AT";
-						rank = "PRIVATE";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_AT";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "PRIVATE";
+					position[] = {15,-15,0};
 				};
-				class FPX_Factions_O_NVA_VC_RaidCell {
-					name = "Vietcong Raider Cell";
+				class Unit6 {
 					side = 0;
-					faction = "FPX_O_NVA";
-					class Unit0 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila";
-						rank = "CORPORAL";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Raider";
-						rank = "PRIVATE";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Raider";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "PRIVATE";
+					position[] = {20,-20,0};
 				};
-				class FPX_Factions_O_NVA_VC_UrbanCell {
-					name = "Vietcong Urban Cell";
+				class Unit7 {
 					side = 0;
-					faction = "FPX_O_NVA";
-					class Unit0 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_Guerrila_AVT";
-						rank = "CORPORAL";
-						position[] = {0,0,0};
-					};
-					class Unit1 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_CQC";
-						rank = "PRIVATE";
-						position[] = {5,-5,0};
-					};
-					class Unit2 {
-						side = 0;
-						vehicle = "FPX_O_NVA_VC_CQC";
-						rank = "PRIVATE";
-						position[] = {-5,-5,0};
-					};
+					vehicle = "FPX_O_NVA_VC_Guerrila_AVT";
+					rank = "PRIVATE";
+					position[] = {-20,-20,0};
+				};
+				class Unit8 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_AT";
+					rank = "PRIVATE";
+					position[] = {-25,-25,0};
+				};
+			};
+			class FPX_Factions_O_NVA_VC_RaidingParty {
+				name = "Vietcong Raiders";
+				side = 0;
+				faction = "FPX_O_NVA";
+				class Unit0 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_CL";
+					rank = "SERGEANT";
+					position[] = {0,0,0};
+				};
+				class Unit1 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "CORPORAL";
+					position[] = {5,-5,0};
+				};
+				class Unit2 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "PRIVATE";
+					position[] = {-5,-5,0};
+				};
+				class Unit3 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "PRIVATE";
+					position[] = {10,-10,0};
+				}; 
+				class Unit4 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Raider";
+					rank = "PRIVATE";
+					position[] = {-10,-10,0};
+				};
+				class Unit5 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Raider";
+					rank = "PRIVATE";
+					position[] = {15,-15,0};
+				};
+				class Unit6 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Raider";
+					rank = "PRIVATE";
+					position[] = {20,-20,0};
+				};
+				class Unit7 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_MG_DP27";
+					rank = "PRIVATE";
+					position[] = {-20,-20,0};
+				};
+				class Unit8 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_AT";
+					rank = "PRIVATE";
+					position[] = {-25,-25,0};
+				};
+			};
+			class FPX_Factions_O_NVA_VC_CityFighters {
+				name = "Vietcong City Fighters";
+				side = 0;
+				faction = "FPX_O_NVA";
+				class Unit0 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_CL";
+					rank = "SERGEANT";
+					position[] = {0,0,0};
+				};
+				class Unit1 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "CORPORAL";
+					position[] = {5,-5,0};
+				};
+				class Unit2 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "PRIVATE";
+					position[] = {-5,-5,0};
+				};
+				class Unit3 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_MG_MG42";
+					rank = "PRIVATE";
+					position[] = {10,-10,0};
+				}; 
+				class Unit4 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_CQC";
+					rank = "PRIVATE";
+					position[] = {-10,-10,0};
+				};
+				class Unit5 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_CQC";
+					rank = "PRIVATE";
+					position[] = {15,-15,0};
+				};
+				class Unit6 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_CQC";
+					rank = "PRIVATE";
+					position[] = {20,-20,0};
+				};
+				class Unit7 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_AT";
+					rank = "PRIVATE";
+					position[] = {-20,-20,0};
+				};
+				class Unit8 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_AT";
+					rank = "PRIVATE";
+					position[] = {-25,-25,0};
+				};
+			};
+			class FPX_Factions_O_NVA_VC_Cell {
+				name = "Vietcong Cell";
+				side = 0;
+				faction = "FPX_O_NVA";
+				class Unit0 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "CORPORAL";
+					position[] = {0,0,0};
+				};
+				class Unit1 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_MG_DP27";
+					rank = "PRIVATE";
+					position[] = {5,-5,0};
+				};
+				class Unit2 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila";
+					rank = "PRIVATE";
+					position[] = {-5,-5,0};
+				};
+			};
+			class FPX_Factions_O_NVA_VC_ATCell {
+				name = "Vietcong AT Cell";
+				side = 0;
+				faction = "FPX_O_NVA";
+				class Unit0 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila_AKM";
+					rank = "CORPORAL";
+					position[] = {0,0,0};
+				};
+				class Unit1 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_AT";
+					rank = "PRIVATE";
+					position[] = {5,-5,0};
+				};
+				class Unit2 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_AT";
+					rank = "PRIVATE";
+					position[] = {-5,-5,0};
+				};
+			};
+			class FPX_Factions_O_NVA_VC_RaidCell {
+				name = "Vietcong Raider Cell";
+				side = 0;
+				faction = "FPX_O_NVA";
+				class Unit0 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila";
+					rank = "CORPORAL";
+					position[] = {0,0,0};
+				};
+				class Unit1 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Raider";
+					rank = "PRIVATE";
+					position[] = {5,-5,0};
+				};
+				class Unit2 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Raider";
+					rank = "PRIVATE";
+					position[] = {-5,-5,0};
+				};
+			};
+			class FPX_Factions_O_NVA_VC_UrbanCell {
+				name = "Vietcong Urban Cell";
+				side = 0;
+				faction = "FPX_O_NVA";
+				class Unit0 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_Guerrila_AVT";
+					rank = "CORPORAL";
+					position[] = {0,0,0};
+				};
+				class Unit1 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_CQC";
+					rank = "PRIVATE";
+					position[] = {5,-5,0};
+				};
+				class Unit2 {
+					side = 0;
+					vehicle = "FPX_O_NVA_VC_CQC";
+					rank = "PRIVATE";
+					position[] = {-5,-5,0};
 				};
 			};
 		};
