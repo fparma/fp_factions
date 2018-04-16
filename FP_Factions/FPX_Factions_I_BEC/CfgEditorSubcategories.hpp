99c5ef698ec9ed7ab8f6565f94f336c4fbd32d0c
Mallow234 <ashilld@sky.com>
2018-04-16 14:38:12 +0100

    Removed useless factions

--- FP_Factions/FPX_Factions_I_BEC/CfgEditorSubcategories.hpp
+++
@@ -1,3 +0,0 @@
-class FPX_EDSubCat_I_BEC_Men_Merc {displayName = "Mercenaries";};
-class FPX_EDSubCat_I_BEC_Heli {displayName = "Helicopters";};
-class FPX_EDSubCat_I_BEC_Car {displayName = "Cars";};
--- FP_Factions/FPX_Factions_O_NVA/CfgEditorSubcategories.hpp
+++
@@ -1,8 +0,0 @@
-class FPX_EDSubCat_O_NVA_Soldiers {displayName = "Men (NVA)";};
-class FPX_EDSubCat_O_NVA_Vietcong {displayName = "Men (VC)";};
-class FPX_EDSubCat_O_NVA_Boat {displayName = "Boats";};
-class FPX_EDSubCat_O_NVA_Car {displayName = "Cars";};
-class FPX_EDSubCat_O_NVA_APC {displayName = "APCs";};
-class FPX_EDSubCat_O_NVA_AA {displayName = "Anti-Air";};
-class FPX_EDSubCat_O_NVA_Tank {displayName = "Tanks";};
-class FPX_EDSubCat_O_NVA_Turret {displayName = "Turrets";};
