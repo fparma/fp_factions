100f26d7ee85a6c5e17c2ce10ffef4fe74637b07
Unknown <Alexander_deguyt@Hotmail.com>
2019-05-24 14:27:39 +0200

    More stoner reskins
    5 new spray camouflages

--- FP_Factions/gordon_reskins/stoner/config.cpp
+++ FP_Factions/gordon_reskins/stoner/config.cpp
@@ -16,6 +16,11 @@ class CfgPatches
 			"FP_GOR_LMG_Mk200_F_carc",
 			"FP_GOR_LMG_Mk200_F_carcdes",
 			"FP_GOR_LMG_Mk200_F_carcwin",
+			"FP_GOR_LMG_Mk200_F_spraywood",
+			"FP_GOR_LMG_Mk200_F_sprayautumn",
+			"FP_GOR_LMG_Mk200_F_spraydes",
+			"FP_GOR_LMG_Mk200_F_spraywin",
+			"FP_GOR_LMG_Mk200_F_sprayurb",
 			"FP_GOR_LMG_Mk200_F_black_greenbox",
 			"FP_GOR_LMG_Mk200_F_black_tanbox",			
 			"FP_GOR_LMG_Mk200_F_black_woodbox"
@@ -139,6 +144,101 @@ class CfgWeapons
 			"\FP_Factions\gordon_reskins\stoner\data\grip_metal.rvmat"
 		}; 
 	};
+	class FP_GOR_LMG_Mk200_F_spraywood: LMG_Mk200_F
+	{
+		author="Gordon Weedman";
+		_generalMacro="FP_GOR_LMG_Mk200_F_spraywood";
+		baseWeapon="FP_GOR_LMG_Mk200_F_spraywood";
+		displayName="Stoner 99 LMG (Spray Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\1st_person_black_greenbox_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\body_spraywoodland_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_black_co.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_1st_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_body_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_metal.rvmat"
+		}; 
+	};
+	class FP_GOR_LMG_Mk200_F_sprayautumn: LMG_Mk200_F
+	{
+		author="Gordon Weedman";
+		_generalMacro="FP_GOR_LMG_Mk200_F_sprayautumn";
+		baseWeapon="FP_GOR_LMG_Mk200_F_sprayautumn";
+		displayName="Stoner 99 LMG (Spray Autumn)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\1st_person_black_greenbox_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\body_sprayautumn_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_black_co.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_1st_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_body_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_metal.rvmat"
+		}; 
+	};
+	class FP_GOR_LMG_Mk200_F_spraydes: LMG_Mk200_F
+	{
+		author="Gordon Weedman";
+		_generalMacro="FP_GOR_LMG_Mk200_F_spraydes";
+		baseWeapon="FP_GOR_LMG_Mk200_F_spraydes";
+		displayName="Stoner 99 LMG (Spray Desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\1st_person_black_tanbox_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\body_spraydesert_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_black_co.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_1st_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_body_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_metal.rvmat"
+		}; 
+	};
+	class FP_GOR_LMG_Mk200_F_spraywin: LMG_Mk200_F
+	{
+		author="Gordon Weedman";
+		_generalMacro="FP_GOR_LMG_Mk200_F_spraywin";
+		baseWeapon="FP_GOR_LMG_Mk200_F_spraywin";
+		displayName="Stoner 99 LMG (Spray Winter)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\1st_person_black_whitebox_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\body_spraywinter_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_black_co.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_1st_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_body_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_metal.rvmat"
+		}; 
+	};
+	class FP_GOR_LMG_Mk200_F_sprayurb: LMG_Mk200_F
+	{
+		author="Gordon Weedman";
+		_generalMacro="FP_GOR_LMG_Mk200_F_sprayurb";
+		baseWeapon="FP_GOR_LMG_Mk200_F_sprayurb";
+		displayName="Stoner 99 LMG (Spray Urban)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\1st_person_black_blackbox_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\body_sprayurban_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_black_co.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_1st_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_body_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_metal.rvmat"
+		}; 
+	};
 	class FP_GOR_LMG_Mk200_F_black_greenbox: LMG_Mk200_F
 	{
 		author="Gordon Weedman";
@@ -177,6 +277,26 @@ class CfgWeapons
 			"\FP_Factions\gordon_reskins\stoner\data\grip_metal.rvmat"
 		}; 
 	};
+	class FP_GOR_LMG_Mk200_F_black_woodbox: LMG_Mk200_F
+	{
+		author="Gordon Weedman";
+		_generalMacro="FP_GOR_LMG_Mk200_F_black_woodbox";
+		baseWeapon="FP_GOR_LMG_Mk200_F_black_woodbox";
+		displayName="Stoner 99 LMG (Black, woodland ammobox)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\1st_person_black_woodbox_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\body_black_co.paa",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_black_co.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_1st_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\vig_m200_body_metal.rvmat",
+			"\FP_Factions\gordon_reskins\stoner\data\grip_metal.rvmat"
+		}; 
+	};
+};
 	class FP_GOR_LMG_Mk200_F_black_woodbox: LMG_Mk200_F
 	{
 		author="Gordon Weedman";
