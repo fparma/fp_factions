a6ad49c9b744b0a9d79b9b0cf5792c5acb3dfaad
Unknown <Alexander_deguyt@Hotmail.com>
2019-09-05 21:15:15 +0200

    biggu commit
    - Added following reskins to Massif uniform and PASGT helmet:
    Oakleaf summer
    Oakleaf fall
    AUSCAM DPCU
    AUSCAM midpoint
    AUSCAM DPDU
    SMK
    CROPAT
    - Added boonie hat reskins with all current camouflages.
    - Added kitbag reskins with all current camouflages.
    - Added LDF uniform reskins with all current camouflages
    - Added uniform reskins with Project Honor camouflage.

---
+++ FP_Factions/gordon_reskins/fastpack/config.cpp
@@ -0,0 +1,190 @@
+class CfgPatches
+{
+	class gordon_reskins_fastpack
+	{
+		// List of units defined in this "PBO"
+		units[]={};
+		weapons[]=
+		{
+			"FP_GOR_fastpack_woodland",
+			"FP_GOR_fastpack_stripeyarid",
+			"FP_GOR_fastpack_stripeydesert",
+			"FP_GOR_fastpack_sripeywoodland",
+			"FP_GOR_fastpack_frenchlizardc1",
+			"FP_GOR_fastpack_frenchlizarda2",
+			"FP_GOR_fastpack_frenchlizardfictional",
+			"FP_GOR_fastpack_tigerstripejungle",
+			"FP_GOR_fastpack_tigerstripedesert",
+			"FP_GOR_fastpack_tigerstripenaval",
+			"FP_GOR_fastpack_oakleafsummer",
+			"FP_GOR_fastpack_oakleaffall",
+			"FP_GOR_fastpack_auscamdpcu",
+			"FP_GOR_fastpack_auscammidpoint",
+			"FP_GOR_fastpack_auscamdpdu",
+			"FP_GOR_fastpack_smk",
+			"FP_GOR_fastpack_cropat"
+		};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_Weapons_F_Ammoboxes"
+		};
+	};
+};
+class CfgVehicles
+{	
+	class B_Kitbag_rgr;
+	class FP_GOR_fastpack_woodland: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (M81 Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_woodland_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_stripeyarid: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (DBC Arid)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_stripeyarid_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_stripeydesert: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (DBC Desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_stripeydesert_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_sripeywoodland: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (DBC Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_stripeywoodland_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_frenchlizardc1: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (French Lizard C1)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_frenchlizardc1_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_frenchlizarda2: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (French Lizard A2)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_frenchlizarda2_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_frenchlizardfictional: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (French Lizard Fictional)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_frenchlizardfictional_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_tigerstripejungle: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (Tigerstripe jungle)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_tigerstripejungle_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_tigerstripedesert: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (Tigerstripe desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_tigerstripedesert_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_tigerstripenaval: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (Tigerstripe naval)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_tigerstripenaval_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_oakleafsummer: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (Oakleaf summer)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_oakleafsummer_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_oakleaffall: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (Oakleaf fall)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_oakleaffall_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_auscamdpcu: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (AUSCAM DPCU)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_auscamdpcu_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_auscammidpoint: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (AUSCAM midpoint)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_auscammidpoint_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_auscamdpdu: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (AUSCAM DPDU)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_auscamdpdu_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_smk: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (SMK)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_smk_co.paa"
+		};
+	};
+	class FP_GOR_fastpack_cropat: B_Kitbag_rgr
+	{
+		author="Gordon Weedman";
+		displayName="Kitbag (CROPAT)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\fastpack\data\fastpack_cropat_co.paa"
+		};
+	};
+}; 
--- FP_Factions/gordon_reskins/pasgthelmet/config.cpp
+++ FP_Factions/gordon_reskins/pasgthelmet/config.cpp
@@ -25,7 +25,21 @@ class CfgPatches
 			"FP_GOR_PASGThelmet_tigerstripedesert",
 			"FP_GOR_PASGThelmet_tigerstripedesert_rhino",
 			"FP_GOR_PASGThelmet_tigerstripenaval",
-			"FP_GOR_PASGThelmet_tigerstripenaval_rhino" 
+			"FP_GOR_PASGThelmet_tigerstripenaval_rhino",
+			"FP_GOR_PASGThelmet_oakleafsummer",
+			"FP_GOR_PASGThelmet_oakleafsummer_rhino",
+			"FP_GOR_PASGThelmet_oakleaffall",
+			"FP_GOR_PASGThelmet_oakleaffall_rhino",
+			"FP_GOR_PASGThelmet_auscamdpcu",
+			"FP_GOR_PASGThelmet_auscamdpcu_rhino",
+			"FP_GOR_PASGThelmet_auscamdpdu",
+			"FP_GOR_PASGThelmet_auscamdpdu_rhino",
+			"FP_GOR_PASGThelmet_auscammidpoint",
+			"FP_GOR_PASGThelmet_auscammidpoint_rhino",
+			"FP_GOR_PASGThelmet_smk",
+			"FP_GOR_PASGThelmet_smk_rhino",
+			"FP_GOR_PASGThelmet_cropat",
+			"FP_GOR_PASGThelmet_cropat_rhino"
 		};
 		requiredVersion=0.1;
 		requiredAddons[]=
@@ -46,7 +60,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (M81 Woodland)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\woodland.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_woodland_co.paa"
@@ -58,7 +71,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (M81 Woodland, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\woodland.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_woodland_co.paa"
@@ -70,7 +82,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (DBC Arid)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\stripeyarid.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_stripeyarid_co.paa"
@@ -82,7 +93,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (DBC Arid, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\stripeyarid.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_stripeyarid_co.paa"
@@ -94,7 +104,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (DBC Desert)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\stripeydesert.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_stripeydesert_co.paa"
@@ -106,7 +115,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (DBC Desert, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\stripeydesert.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_stripeydesert_co.paa"
@@ -118,7 +126,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (DBC Woodland)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\stripeywoodland.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_stripeywoodland_co.paa"
@@ -130,7 +137,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (DBC Woodland, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\stripeywoodland.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_stripeywoodland_co.paa"
@@ -142,7 +148,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (French Lizard C1)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\frenchlizardc1.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_frenchlizardc1_co.paa"
@@ -154,7 +159,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (French Lizard C1, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\frenchlizardc1.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_frenchlizardc1_co.paa"
@@ -166,7 +170,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (French Lizard A2)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\frenchlizarda2.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_frenchlizarda2_co.paa"
@@ -178,7 +181,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (French Lizard A2, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\frenchlizarda2.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_frenchlizarda2_co.paa"
@@ -190,7 +192,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (French Lizard Fictional)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\frenchlizardfictional.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_frenchlizardfictional_co.paa"
@@ -202,7 +203,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (French Lizard Fictional, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\frenchlizardfictional.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_frenchlizardfictional_co.paa"
@@ -214,7 +214,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (Tigerstripe jungle)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\tigerstripejungle.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_tigerstripejungle_co.paa"
@@ -226,7 +225,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (Tigerstripe jungle, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\tigerstripejungle.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_tigerstripejungle_co.paa"
@@ -238,7 +236,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (Tigerstripe desert)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\tigerstripedesert.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_tigerstripedesert_co.paa"
@@ -250,7 +247,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (Tigerstripe desert, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\tigerstripedesert.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_tigerstripedesert_co.paa"
@@ -262,7 +258,6 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl";
 		scope=2;
 		displayName="PASGT (Tigerstripe naval)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\tigerstripenaval.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_tigerstripenaval_co.paa"
@@ -274,10 +269,163 @@ class CfgWeapons
 		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
 		scope=2;
 		displayName="PASGT (Tigerstripe naval, Rhino)";
-		picture="\FP_Factions\gordon_reskins\universalstuff\icons\uniforms\tigerstripenaval.paa";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_tigerstripenaval_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_oakleafsummer : rhsgref_helmet_pasgt_erdl
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl";
+		scope=2;
+		displayName="PASGT (Oakleaf summer)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_oakleafsummer_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_oakleafsummer_rhino : rhsgref_helmet_pasgt_erdl_rhino
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
+		scope=2;
+		displayName="PASGT (Oakleaf summer, Rhino)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_oakleafsummer_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_oakleaffall : rhsgref_helmet_pasgt_erdl
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl";
+		scope=2;
+		displayName="PASGT (Oakleaf fall)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_oakleaffall_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_oakleaffall_rhino : rhsgref_helmet_pasgt_erdl_rhino
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
+		scope=2;
+		displayName="PASGT (Oakleaf fall, Rhino)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_oakleaffall_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_auscamdpcu : rhsgref_helmet_pasgt_erdl
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl";
+		scope=2;
+		displayName="PASGT (AUSCAM DPCU)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_auscamdpcu_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_auscamdpcu_rhino : rhsgref_helmet_pasgt_erdl_rhino
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
+		scope=2;
+		displayName="PASGT (AUSCAM DPCU, Rhino)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_auscamdpcu_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_auscamdpdu : rhsgref_helmet_pasgt_erdl
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl";
+		scope=2;
+		displayName="PASGT (AUSCAM DPDU)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_auscamdpdu_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_auscamdpdu_rhino : rhsgref_helmet_pasgt_erdl_rhino
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
+		scope=2;
+		displayName="PASGT (AUSCAM DPDU, Rhino)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_auscamdpdu_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_auscammidpoint : rhsgref_helmet_pasgt_erdl
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl";
+		scope=2;
+		displayName="PASGT (AUSCAM midpoint)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_auscammidpoint_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_auscammidpoint_rhino : rhsgref_helmet_pasgt_erdl_rhino
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
+		scope=2;
+		displayName="PASGT (AUSCAM midpoint, Rhino)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_auscammidpoint_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_smk : rhsgref_helmet_pasgt_erdl
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl";
+		scope=2;
+		displayName="PASGT (SMK)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_smk_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_smk_rhino : rhsgref_helmet_pasgt_erdl_rhino
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
+		scope=2;
+		displayName="PASGT (SMK, Rhino)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_smk_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_cropat : rhsgref_helmet_pasgt_erdl
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl";
+		scope=2;
+		displayName="PASGT (CROPAT)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_cropat_co.paa"
+		};
+	};
+	class FP_GOR_PASGThelmet_cropat_rhino : rhsgref_helmet_pasgt_erdl_rhino
+	{
+		author="Gordon Weedman";
+		_generalMacro="rhsgref_helmet_pasgt_erdl_rhino";
+		scope=2;
+		displayName="PASGT (CROPAT, Rhino)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\pasgthelmet\data\pasgt_helmet_cropat_co.paa"
 		};
 	};
 };
