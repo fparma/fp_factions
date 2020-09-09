3871f665e82011a55cf221901ebdec9464eb75da
Unknown <Alexander_deguyt@Hotmail.com>
2020-09-09 19:59:59 +0200

    Fixed minor PASGT config error, added Carryall Backpack skins
    Also improved vanilla Carryall skin (green hex)

---
+++ FP_Factions/gordon_reskins/tortila/config.cpp
@@ -0,0 +1,331 @@
+class CfgPatches
+{
+	class gordon_reskins_tortila
+	{
+		// List of units defined in this "PBO"
+		units[]={};
+		weapons[]=
+		{
+			"FP_GOR_tortila_woodland",
+			"FP_GOR_tortila_stripeyarid",
+			"FP_GOR_tortila_stripeydesert",
+			"FP_GOR_tortila_sripeywoodland",
+			"FP_GOR_tortila_frenchlizardc1",
+			"FP_GOR_tortila_frenchlizarda2",
+			"FP_GOR_tortila_frenchlizardfictional",
+			"FP_GOR_tortila_tigerstripejungle",
+			"FP_GOR_tortila_tigerstripedesert",
+			"FP_GOR_tortila_tigerstripenaval",
+			"FP_GOR_tortila_oakleafsummer",
+			"FP_GOR_tortila_oakleaffall",
+			"FP_GOR_tortila_auscamdpcu",
+			"FP_GOR_tortila_auscamdpdu",
+			"FP_GOR_tortila_smk",
+			"FP_GOR_tortila_cropat",
+			"FP_GOR_tortila_cropatdesert",
+			"FP_GOR_tortila_cs95dpm",
+			"FP_GOR_tortila_pantera",
+			"FP_GOR_tortila_panteradesert",
+			"FP_GOR_tortila_vz95woodland",
+			"FP_GOR_tortila_vz95desert",
+			"FP_GOR_tortila_flecktarnsummer",
+			"FP_GOR_tortila_flecktarnautumn",
+			"FP_GOR_tortila_flecktarndesert",
+			"FP_GOR_tortila_mm14",
+			"FP_GOR_tortila_mm14woodland",
+			"FP_GOR_tortila_honor",
+			"FP_GOR_tortila_m90",
+			"FP_GOR_tortila_m90k",
+			"B_Carryall_ghex_F"
+		};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_Weapons_F_Ammoboxes",
+			"A3_Data_F_Oldman_Loadorder"
+		};
+	};
+};
+class CfgVehicles
+{	
+	class B_Carryall_oli;
+	class FP_GOR_tortila_woodland: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (M81 Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_woodland_co.paa"
+		};
+	};
+	class FP_GOR_tortila_stripeyarid: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (DBC Arid)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_stripeyarid_co.paa"
+		};
+	};
+	class FP_GOR_tortila_stripeydesert: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (DBC Desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_stripeydesert_co.paa"
+		};
+	};
+	class FP_GOR_tortila_sripeywoodland: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (DBC Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_stripeywoodland_co.paa"
+		};
+	};
+	class FP_GOR_tortila_frenchlizardc1: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (French Lizard C1)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_frenchlizardc1_co.paa"
+		};
+	};
+	class FP_GOR_tortila_frenchlizarda2: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (French Lizard A2)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_frenchlizarda2_co.paa"
+		};
+	};
+	class FP_GOR_tortila_frenchlizardfictional: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (French Lizard Fictional)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_frenchlizardfictional_co.paa"
+		};
+	};
+	class FP_GOR_tortila_tigerstripejungle: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Tigerstripe jungle)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_tigerstripejungle_co.paa"
+		};
+	};
+	class FP_GOR_tortila_tigerstripedesert: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Tigerstripe desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_tigerstripedesert_co.paa"
+		};
+	};
+	class FP_GOR_tortila_tigerstripenaval: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Tigerstripe naval)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_tigerstripenaval_co.paa"
+		};
+	};
+	class FP_GOR_tortila_oakleafsummer: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Oakleaf summer)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_oakleafsummer_co.paa"
+		};
+	};
+	class FP_GOR_tortila_oakleaffall: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Oakleaf fall)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_oakleaffall_co.paa"
+		};
+	};
+	class FP_GOR_tortila_auscamdpcu: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (AUSCAM DPCU)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_auscamdpcu_co.paa"
+		};
+	};
+	class FP_GOR_tortila_auscamdpdu: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (AUSCAM DPDU)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_auscamdpdu_co.paa"
+		};
+	};
+	class FP_GOR_tortila_smk: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (SMK)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_smk_co.paa"
+		};
+	};
+	class FP_GOR_tortila_cropat: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (CROPAT)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_cropat_co.paa"
+		};
+	};
+	class FP_GOR_tortila_cropatdesert: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (CROPAT Desert)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_cropatdesert_co.paa"
+		};
+	};
+	class FP_GOR_tortila_cs95dpm: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (DPM CS95)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_cs95dpm_co.paa"
+		};
+	};
+	class FP_GOR_tortila_pantera: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Polish Pantera)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_pantera_co.paa"
+		};
+	};
+	class FP_GOR_tortila_panteradesert: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Polish Pantera (Desert))";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_panteradesert_co.paa"
+		};
+	};
+	class FP_GOR_tortila_vz95woodland: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Czech Vz95)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_vz95woodland_co.paa"
+		};
+	};
+	class FP_GOR_tortila_vz95desert: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Czech Vz95 (Desert))";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_vz95desert_co.paa"
+		};
+	};
+	class FP_GOR_tortila_flecktarnsummer: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Flecktarn (Summer))";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_flecktarnsummer_co.paa"
+		};
+	};
+	class FP_GOR_tortila_flecktarnautumn: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Flecktarn (Autumn))";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_flecktarnautumn_co.paa"
+		};
+	};
+	class FP_GOR_tortila_flecktarndesert: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Flecktarn (Desert))";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_flecktarndesert_co.paa"
+		};
+	};
+	class FP_GOR_tortila_mm14: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Ukrainian MM-14)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_mm14_co.paa"
+		};
+	};
+	class FP_GOR_tortila_mm14woodland: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Ukrainian MM-14 Woodland)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_mm14woodland_co.paa"
+		};
+	};
+	class FP_GOR_tortila_honor: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Project Honor)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_honor_co.paa"
+		};
+	};
+	class FP_GOR_tortila_m90: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Swedish M90)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_m90_co.paa"
+		};
+	};
+	class FP_GOR_tortila_m90k: B_Carryall_oli
+	{
+		author="Gordon Weedman";
+		displayName="Carryall Backpack (Swedish M90K)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_m90k_co.paa"
+		};
+	};
+	class B_Carryall_Base;
+	class B_Carryall_ghex_F: B_Carryall_Base
+	{
+		author="Bohemia Interactive & Gordon Weedman";		
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\tortila\data\backpack_tortila_ghex_co.paa"
+		};
+	};
+}; 
