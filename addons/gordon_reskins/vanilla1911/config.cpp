ef1cea774491e587cfcbacb63175223e2619ab1d
Gordon Weedman <Alexander_deguyt@Hotmail.com>
2022-09-04 15:28:25 +0200

    few more 1911s

--- addons/gordon_reskins/vanilla1911/config.cpp
+++ addons/gordon_reskins/vanilla1911/config.cpp
@@ -14,7 +14,9 @@ class CfgPatches
 			"FP_GOR_hgun_ACPC2_green_F",
 			"FP_GOR_hgun_ACPC2_fde_F",
 			"FP_GOR_hgun_ACPC2_fdeslide_F",
-			"FP_GOR_hgun_ACPC2_chromereceiver_F"
+			"FP_GOR_hgun_ACPC2_chromeslide_F",
+			"FP_GOR_hgun_ACPC2_chromereceiver_F",
+			"FP_GOR_hgun_ACPC2_chrome_F"
 		};
 	};
 };
@@ -70,12 +72,27 @@ class CfgWeapons
 			"\FP_Factions\gordon_reskins\vanilla1911\data\Acpc2_slide.rvmat"
 		}; 
 	};
+	class FP_GOR_hgun_ACPC2_chromeslide_F: hgun_ACPC2_F
+	{
+		author="Gordon Weedman";
+		_generalMacro="FP_GOR_hgun_ACPC2_chromeslide_F";
+		baseWeapon="FP_GOR_hgun_ACPC2_chromeslide_F";
+		displayName="Custom Covert II (Two-tone)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\vanilla1911\data\Acpc2_chromeslide_co.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"\FP_Factions\gordon_reskins\vanilla1911\data\Acpc2_chromeslide.rvmat"
+		}; 
+	};
 	class FP_GOR_hgun_ACPC2_chromereceiver_F: hgun_ACPC2_F
 	{
 		author="Gordon Weedman";
 		_generalMacro="FP_GOR_hgun_ACPC2_chromereceiver_F";
 		baseWeapon="FP_GOR_hgun_ACPC2_chromereceiver_F";
-		displayName="Custom Covert II (2-tone)";
+		displayName="Custom Covert II (Two-tone inverted)";
 		hiddenSelectionsTextures[]=
 		{
 			"\FP_Factions\gordon_reskins\vanilla1911\data\Acpc2_chromereceiver_co.paa"
@@ -83,6 +100,21 @@ class CfgWeapons
 		hiddenSelectionsMaterials[]=
 		{
 			"\FP_Factions\gordon_reskins\vanilla1911\data\Acpc2_chromereceiver.rvmat"
+		}; 
+	};
+	class FP_GOR_hgun_ACPC2_chrome_F: hgun_ACPC2_F
+	{
+		author="Gordon Weedman";
+		_generalMacro="FP_GOR_hgun_ACPC2_chrome_F";
+		baseWeapon="FP_GOR_hgun_ACPC2_chrome_F";
+		displayName="Custom Covert II (Chrome)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\vanilla1911\data\Acpc2_chrome_co.paa"
+		};
+		hiddenSelectionsMaterials[]=
+		{
+			"\FP_Factions\gordon_reskins\vanilla1911\data\Acpc2_chrome.rvmat"
 		}; 
 	};
 };
