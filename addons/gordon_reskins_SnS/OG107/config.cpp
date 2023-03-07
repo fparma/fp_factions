1d10076ea41e1bc4e8108326e8995d4599ed19ff
Gordon Weedman <Alexander_deguyt@Hotmail.com>
2023-03-07 12:44:00 +0100

    Added Polish Worm and Strichtarn camos to OG-107s and optimized

--- addons/gordon_reskins_SnS/OG107/config.cpp
+++ addons/gordon_reskins_SnS/OG107/config.cpp
@@ -101,7 +101,15 @@ class CfgPatches
 			"FP_GOR_Uniform_og107_mk3_rhodesianexperimental_blench",
 			"FP_GOR_Uniform_og107_mk3_rhodesianexperimental_trop_blench",
 			"FP_GOR_Uniform_og107_mk3_rhodesianexperimental_tuck_blench",
-			"FP_GOR_Uniform_og107_mk3_rhodesianexperimental_tuck_trop_blench"
+			"FP_GOR_Uniform_og107_mk3_rhodesianexperimental_tuck_trop_blench",
+			"FP_GOR_Uniform_og107_mk3_strichtarn_blench",
+			"FP_GOR_Uniform_og107_mk3_strichtarn_trop_blench",
+			"FP_GOR_Uniform_og107_mk3_strichtarn_tuck_blench",
+			"FP_GOR_Uniform_og107_mk3_strichtarn_tuck_trop_blench",
+			"FP_GOR_Uniform_og107_mk3_polishworm_blench",
+			"FP_GOR_Uniform_og107_mk3_polishworm_trop_blench",
+			"FP_GOR_Uniform_og107_mk3_polishworm_tuck_blench",
+			"FP_GOR_Uniform_og107_mk3_polishworm_tuck_trop_blench"
 		};
 		requiredVersion=0.1;
 		requiredAddons[]=
