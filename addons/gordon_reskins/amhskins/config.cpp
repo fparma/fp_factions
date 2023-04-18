59d98b6cfd46c0cde8183f5092bbcab65cb4a0d7
Gordon Weedman <Alexander_deguyt@Hotmail.com>
2023-04-18 22:26:20 +0200

    Added DBC to AMH, tweaked AAF and Woodland skins

--- addons/gordon_reskins/amhskins/config.cpp
+++ addons/gordon_reskins/amhskins/config.cpp
@@ -25,7 +25,18 @@ class CfgPatches
 			"FP_GOR_H_HelmetHBK_Woodland_F",
 			"FP_GOR_H_HelmetHBK_headset_Woodland_F",
 			"FP_GOR_H_HelmetHBK_ear_Woodland_F",
-			"FP_GOR_H_HelmetHBK_chops_Woodland_F"
+			"FP_GOR_H_HelmetHBK_chops_stripeywoodland_F",
+			"FP_GOR_H_HelmetHBK_headset_stripeywoodland_F",
+			"FP_GOR_H_HelmetHBK_ear_stripeywoodland_F",
+			"FP_GOR_H_HelmetHBK_chops_stripeywoodland_F",
+			"FP_GOR_H_HelmetHBK_chops_stripeyarid_F",
+			"FP_GOR_H_HelmetHBK_headset_stripeyarid_F",
+			"FP_GOR_H_HelmetHBK_ear_stripeyarid_F",
+			"FP_GOR_H_HelmetHBK_chops_stripeyarid_F",
+			"FP_GOR_H_HelmetHBK_chops_stripeydesert_F",
+			"FP_GOR_H_HelmetHBK_headset_stripeydesert_F",
+			"FP_GOR_H_HelmetHBK_ear_stripeydesert_F",
+			"FP_GOR_H_HelmetHBK_chops_stripeydesert_F"
 		};
 		requiredVersion=0.1;
 		requiredAddons[]=
@@ -231,5 +242,126 @@ class CfgWeapons
 		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
 	};
 	
+	class FP_GOR_H_HelmetHBK_stripeywoodland_F: H_HelmetHBK_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_stripeywoodland_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Woodland)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeywoodland_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	class FP_GOR_H_HelmetHBK_headset_stripeywoodland_F: H_HelmetHBK_headset_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_headset_stripeywoodland_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Woodland, headset)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeywoodland_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	class FP_GOR_H_HelmetHBK_ear_stripeywoodland_F: H_HelmetHBK_ear_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_ear_stripeywoodland_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Woodland, ear protectors)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeywoodland_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	class FP_GOR_H_HelmetHBK_chops_stripeywoodland_F: H_HelmetHBK_chops_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_chops_stripeywoodland_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Woodland, chops)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeywoodland_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
 	
+	class FP_GOR_H_HelmetHBK_stripeyarid_F: H_HelmetHBK_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_stripeyarid_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Arid)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeyarid_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	class FP_GOR_H_HelmetHBK_headset_stripeyarid_F: H_HelmetHBK_headset_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_headset_stripeyarid_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Arid, headset)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeyarid_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	class FP_GOR_H_HelmetHBK_ear_stripeyarid_F: H_HelmetHBK_ear_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_ear_stripeyarid_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Arid, ear protectors)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeyarid_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	class FP_GOR_H_HelmetHBK_chops_stripeyarid_F: H_HelmetHBK_chops_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_chops_stripeyarid_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Arid, chops)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeyarid_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	
+	class FP_GOR_H_HelmetHBK_stripeydesert_F: H_HelmetHBK_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_stripeydesert_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Desert)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeydesert_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	class FP_GOR_H_HelmetHBK_headset_stripeydesert_F: H_HelmetHBK_headset_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_headset_stripeydesert_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Desert, headset)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeydesert_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	class FP_GOR_H_HelmetHBK_ear_stripeydesert_F: H_HelmetHBK_ear_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_ear_stripeydesert_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Desert, ear protectors)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeydesert_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
+	class FP_GOR_H_HelmetHBK_chops_stripeydesert_F: H_HelmetHBK_chops_F
+	{
+		author = "Gordon Weedman";
+		_generalMacro = "FP_GOR_H_HelmetHBK_chops_stripeydesert_F";
+		scope = 2;
+		displayName = "Advanced Modular Helmet (DBC Desert, chops)";
+		picture = "\a3\Characters_F_Enoch\Headgear\data\UI\icon_H_HelmetHBK_F_CA.paa";
+		hiddenSelectionsTextures[] = {"\FP_Factions\gordon_reskins\amhskins\data\ModHelmet_stripeydesert_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\FP_Factions\gordon_reskins\amhskins\data\H_HelmetHBK_01.rvmat"};
+	};
 };
