9ee17f01d84545aae40ab6c1f33f44032136b7b2
Gordon Weedman <Alexander_deguyt@Hotmail.com>
2024-07-14 19:37:17 +0200

    Added extra details and more patterns to Korzar

--- addons/gordon_reskins/korzar/config.cpp
+++ addons/gordon_reskins/korzar/config.cpp
@@ -13,7 +13,15 @@ class CfgPatches
 			"FP_GOR_Korzar3_spets",
 			"FP_GOR_Korzar3_spets_B",
 			"FP_GOR_Korzar3_spets_P",
-			"FP_GOR_Korzar3_spets_R"
+			"FP_GOR_Korzar3_spets_R",
+			"FP_GOR_Korzar3_slivawood",
+			"FP_GOR_Korzar3_slivawood_B",
+			"FP_GOR_Korzar3_slivawood_P",
+			"FP_GOR_Korzar3_slivawood_R",
+			"FP_GOR_Korzar3_slivasteppe",
+			"FP_GOR_Korzar3_slivasteppe_B",
+			"FP_GOR_Korzar3_slivasteppe_P",
+			"FP_GOR_Korzar3_slivasteppe_R"
 		};
 		requiredVersion=0.1;
 		requiredAddons[]=
@@ -132,6 +140,112 @@ class CfgWeapons
 			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_General_Pouch_co.paa",
 			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_Radio_Pouch_co.paa",
 			"\MSA_CDF_Korzar\data\bandolier\camo\Tan\MSA_UGL_Bandolier_co.paa"
+		};
+	};
+	
+	class FP_GOR_Korzar3_slivawood : FP_Korzar3_Olive
+	{
+		author="Gordon Weedman";
+		scope=2;
+		displayName="Korzar-3 (Sliva (woodland))";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\korzar\data\korzar3_slivawood_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_General_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_Radio_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\bandolier\camo\Olive\MSA_UGL_Bandolier_co.paa"
+		};
+	};
+	class FP_GOR_Korzar3_slivawood_B: FP_Korzar3_Olive_B
+	{
+		author="Gordon Weedman";
+		scope=2;
+		displayName="Korzar-3 (Sliva (woodland), Bandolier)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\korzar\data\korzar3_slivawood_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_General_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_Radio_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\bandolier\camo\Olive\MSA_UGL_Bandolier_co.paa"
+		};
+	};
+	class FP_GOR_Korzar3_slivawood_P: FP_Korzar3_Olive_P
+	{
+		author="Gordon Weedman";
+		scope=2;
+		displayName="Korzar-3 (Sliva (woodland), Pouch)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\korzar\data\korzar3_slivawood_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_General_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_Radio_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\bandolier\camo\Olive\MSA_UGL_Bandolier_co.paa"
+		};
+	};
+	class FP_GOR_Korzar3_slivawood_R: FP_Korzar3_Olive_R
+	{
+		author="Gordon Weedman";
+		scope=2;
+		displayName="Korzar-3 (Sliva (woodland), Radio)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\korzar\data\korzar3_slivawood_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_General_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_Radio_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\bandolier\camo\Olive\MSA_UGL_Bandolier_co.paa"
+		};
+	};
+	
+	class FP_GOR_Korzar3_slivasteppe : FP_Korzar3_Olive
+	{
+		author="Gordon Weedman";
+		scope=2;
+		displayName="Korzar-3 (Sliva (steppe))";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\korzar\data\korzar3_slivasteppe_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_General_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_Radio_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\bandolier\camo\Olive\MSA_UGL_Bandolier_co.paa"
+		};
+	};
+	class FP_GOR_Korzar3_slivasteppe_B: FP_Korzar3_Olive_B
+	{
+		author="Gordon Weedman";
+		scope=2;
+		displayName="Korzar-3 (Sliva (steppe), Bandolier)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\korzar\data\korzar3_slivasteppe_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_General_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_Radio_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\bandolier\camo\Olive\MSA_UGL_Bandolier_co.paa"
+		};
+	};
+	class FP_GOR_Korzar3_slivasteppe_P: FP_Korzar3_Olive_P
+	{
+		author="Gordon Weedman";
+		scope=2;
+		displayName="Korzar-3 (Sliva (steppe), Pouch)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\korzar\data\korzar3_slivasteppe_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_General_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_Radio_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\bandolier\camo\Olive\MSA_UGL_Bandolier_co.paa"
+		};
+	};
+	class FP_GOR_Korzar3_slivasteppe_R: FP_Korzar3_Olive_R
+	{
+		author="Gordon Weedman";
+		scope=2;
+		displayName="Korzar-3 (Sliva (steppe), Radio)";
+		hiddenSelectionsTextures[]=
+		{
+			"\FP_Factions\gordon_reskins\korzar\data\korzar3_slivasteppe_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_General_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\pouch\camo\Olive\MSA_Radio_Pouch_co.paa",
+			"\MSA_CDF_Korzar\data\bandolier\camo\Olive\MSA_UGL_Bandolier_co.paa"
 		};
 	};
 };
