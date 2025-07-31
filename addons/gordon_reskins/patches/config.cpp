e609cb13424b34f15709fc303a12688f69a785d2
Gordon Weedman <Alexander_deguyt@Hotmail.com>
2025-07-31 21:56:58 +0200

    Medic patches
    Medic patches

---
+++ addons/gordon_reskins/patches/config.cpp
@@ -0,0 +1,52 @@
+class CfgPatches
+{
+	class gordon_reskins_patches
+	{
+		// List of units defined in this "PBO"
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			
+		};
+	};
+};
+class CfgUnitInsignia
+{
+	class medicolivesubdued
+	{
+		displayName = "Medic (Olive subdued)";
+		author = "Gordon Weedman";
+		texture = "\fp_factions\gordon_reskins\patches\data\medicpatcholivesubdued_ca.paa";
+		textureVehicle = "";
+	};
+	class medicolive
+	{
+		displayName = "Medic (Olive)";
+		author = "Gordon Weedman";
+		texture = "\fp_factions\gordon_reskins\patches\data\medicpatcholive_ca.paa";
+		textureVehicle = "";
+	};
+	class medictansubdued
+	{
+		displayName = "Medic (Tan subdued)";
+		author = "Gordon Weedman";
+		texture = "\fp_factions\gordon_reskins\patches\data\medicpatchtansubdued_ca.paa";
+		textureVehicle = "";
+	};
+	class medictan
+	{
+		displayName = "Medic (Tan)";
+		author = "Gordon Weedman";
+		texture = "\fp_factions\gordon_reskins\patches\data\medicpatchtan_ca.paa";
+		textureVehicle = "";
+	};
+	class medicwhite
+	{
+		displayName = "Medic";
+		author = "Gordon Weedman";
+		texture = "\fp_factions\gordon_reskins\patches\data\medicpatchwhite_ca.paa";
+		textureVehicle = "";
+	};
+};
